79acb6fdc036145e74f1bf437bbf5f92dcb6d8a7
Jblt <57819528+Zanic23@users.noreply.github.com>
2022-01-19 17:56:46 -0500

    forgot to add a file

---
+++ server/nft-sources.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "opensea" : {
+        name : "name", 
+        description : "description", 
+        image : "image"
+    }, 
+    "cryptokitties" : {
+        name : "name", 
+        description : "bio", 
+        image : "image_url"
+    },
+    "moralis" : {
+        name : "name", 
+        description : "description", 
+        image : "image"
+    },
+    "larvalabs" : {
+        name : "name", 
+        description : "description", 
+        image : "image"
+    }
+};
