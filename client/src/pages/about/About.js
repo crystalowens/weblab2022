7477f66d01b78e4ad2b7f525e9213d6e50f0804d
Crystal Owens <cryst@DESKTOP-FLL09SG.mit.edu>
2022-01-25 22:12:30 -0500

    more text on about page

--- client/src/pages/about/About.js
+++ client/src/pages/about/About.js
@@ -1,6 +1,8 @@
 
 import React from "react";
 import NavBar from "../../components/navigation/NavBar.js";
+import img6 from "../../resources/images/bored-ape-6.jpg";
+// import img1 from "http://assets.nftrade.com/image/upload/w_500,c_scale/v1628952917/evm_1_0xc1caf0c19a8ac28c41fe59ba6c754e4b9bd54de9_439.png"; 
 
 import "./About.css";
 import "../../util/utilities.js";
@@ -15,7 +17,7 @@ const About = () => {
         <div className="u-flex">
             <div className="About-subContainer u-textCenter">
               <h4 className="About-title">Imagine</h4>
-              <div id="About-text">
+              <div className="About-text">
                   <div>
                     <p>… a world defined by different currencies</p>
                     <p>… a fully digital art environment</p>
@@ -23,11 +25,15 @@ const About = () => {
                   </div>
               </div>
               <h4 className="About-title">Would you survive?</h4>
-              <div id="About-text">
+              <div>
+                <div className="About-text Padded-left">
                   <div>
-                    <p>This site aims to familiarize you with NFTs (non-fungible tokens, typically static or dynamic art sold on the blockchain). By understanding how different art is priced and compared, you are equipped to . </p>
-                     <p> Some tips for the game:</p> 
-                      <p> you'll notice certain categories are systematically more valuable. </p> 
+                    <p>This site aims to familiarize you with NFTs, or "non-fungible tokens," which are typically static or dynamic art sold on the blockchain. By understanding how different art is priced and compared, you are equipped to better deal with our digital future. The most expensive NFT ever sold was Pak's "The Merge," sold in 2021 for over USD$91m, but art evaluated on this site is more typically between USD $25 (0.01 ETH as of Jan 2022) and USD $73,000 (30 ETH). What's the most expensive NFT you've seen?</p>
+                     <p> Some quick tips for the game:</p> 
+                      <p> - You'll notice certain categories are systematically more valuable, and it's not based on aesthetic appeal. </p> 
+                      <p> - Within a category, interesting and bright details add value. </p> 
+                      <p> - Look for other typical traits to score more points and reach a new high score! </p> 
+                  </div>
                   </div>
               </div>
 
